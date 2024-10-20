5eb7b661f686dd186d6becce598693f2e10e18e7
Weng Chong (Billy) LAO <wengchonglao0124@gmail.com>
2024-10-20 17:24:50 +1000

    Initial Commit

---
+++ ObjectExtractor/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  ObjectExtractor
+//
+//  Created by weng chong lao on 20/10/2024.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+
+            ToggleImmersiveSpaceButton()
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+        .environment(AppModel())
+}
---
+++ ObjectExtractor/ObjectExtractorApp.swift
@@ -0,0 +1,33 @@
+//
+//  ObjectExtractorApp.swift
+//  ObjectExtractor
+//
+//  Created by weng chong lao on 20/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ObjectExtractorApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
